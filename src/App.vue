75fa82e7eda4579e79401b93040315401949ffae
Guy Duchatelet <gduchatelet@alertlogic.com>
2021-10-20 11:30:32 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/groceries.png" />
+  <TheList
+    name="Saved Profile"
+    direction="source"
+    :items="profile_items"
+    @item-select="addItem"
+  />
+  <TheList
+    name="List for Today"
+    direction="destination"
+    :items="current_items"
+    @item-select="removeItem"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TheList from "./components/TheList.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TheList,
+  },
+  data() {
+    return {
+      lists: {
+        profile: [
+          { name: "chicken strips", nr: 5 },
+          { name: "wok veggies", nr: 3 },
+          { name: "lemons", nr: 5 },
+        ],
+        current: [],
+      },
+    };
+  },
+  methods: {
+    addItem(item, list) {
+      const itemIdx = this.lists[list].findIndex(
+        (element) => element.name === item.name
+      );
+
+      const currentItemIdx = this.current_items.findIndex(
+        (element) => element.name === name
+      );
+
+      if (currentItemIdx === -1) {
+        this.current_items.push({
+          name: this.profile_items[itemIdx].name,
+          nr: this.profile_items[itemIdx].nr,
+        });
+      } else {
+        this.current_items[currentItemIdx].nr =
+          this.current_items[currentItemIdx].nr +
+          this.profile_items[itemIdx].nr;
+      }
+    },
+
+    removeItem(name) {
+      const itemIdx = this.current_items.findIndex(
+        (element) => element.name === name
+      );
+      this.current_items.splice(itemIdx, 1);
+    },
+  },
+};
 </script>
 
 <style>
@@ -19,8 +70,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/TheList.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>{{ name }}: {{ items.length }} items</h1>
+  <ul>
+    <li v-for="item in items" :key="item.name">
+      <span> {{ item.name }}: </span>
+      <span> {{ item.nr }} </span>
+      <button @click.prevent="$emit('item-select',item.name)"> {{action}} </button>
+      <button @click.prevent="$emit('item-increase',item.name)"> + </button>
+      <button @click.prevent="$emit('item-decrease',item.name)"> - </button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ["name", "items", "direction"],
+  emits: ['item-select'],
+  computed: {
+      action() {
+          return (this.direction==='source'?'Add':'Remove')
+      }
+  }
+};
+</script>
+<style scoped>
+h1 {
+  font-size: 1rem;
+}
+button {
+	font-family: "Verdana" sans-serif;
+	font-size: 0.8em;
+	text-align: middle;
+	color: blue;
+	background: none;
+	margin: 0;
+	padding: 0 0 0 10px;
+	border: none;
+	cursor: pointer;
+}
+</style>
