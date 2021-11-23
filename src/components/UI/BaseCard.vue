4ec41a30945e38d94cd9f9b75c1b37d113505761
Guy Duchatelet <guy.duchatelet@gmail.com>
2021-11-22 23:06:17 -0600

    🚸  Style improvements Fixes #7

--- src/components/UI/BaseCard.vue
+++ src/components/UI/BaseCard.vue
@@ -1,17 +1,50 @@
 <template>
-  <div>
-    <slot></slot>
+  <div class="card">
+    <div class="card-header">
+      <slot name="header"></slot>
+    </div>
+    <div class="card-body">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <style scoped>
 div {
-  margin: 1rem 1rem;
+  box-sizing: border-box;
+}
+div.card {
+  display: inline-block;
+  vertical-align: top;
+  margin: 0.5rem;
   max-width: 40%;
-  padding: 1rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  background-color: rgb(233, 230, 224);
+  padding: 0rem;
+  box-sizing: border-box;
+  border-radius: 10px 10px 10px 10px;
+  border: 1px solid rgb(208, 215, 222);
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.112);
+  background-color: rgba(255, 255, 255, 0.892);
+}
 
+div.card-header {
+  border-bottom: solid 1px rgb(208, 215, 222);
+  margin: 0;
+  padding: 5px 10px;
+  background: #f6f8fa;
+  border-radius: 10px 10px 0px 0px;
+  box-sizing: border-box;
+}
+
+.standard {
+  height: 45px;
+}
+.extended {
+  height: 60px;
+}
+
+div.card-body {
+  margin: 0;
+  padding: 0rem 0rem;
+  box-sizing: border-box;
 }
 </style>
