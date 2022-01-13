d9efd96bcd8b67cbcacfa2c30cf715287e7e624c
Guy Duchatelet <guy.duchatelet@gmail.com>
2022-01-12 18:07:48 -0600

    exclud integration tests

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,5 @@ module.exports = {
   transform: {
     "^.+\\.vue$": "vue-jest",
   },
-  testMatch: [
-    "**/tests/unit/**/*.spec.[jt]s?(x)",
-    "**/tests/integration/**/*.spec.[jt]s?(x)",
-  ],
+  testMatch: ["**/tests/unit/**/*.spec.[jt]s?(x)"],
 };
